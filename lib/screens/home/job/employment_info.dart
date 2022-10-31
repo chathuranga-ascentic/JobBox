25c8fc6e7e817412431f4f9286ca54b9e08d014b
Chathuranga <chathuranga@Chathurangas-MacBook-Pro.local>
2022-10-31 11:01:17 +0530

    update UI

--- lib/screens/home/job/employment_info.dart
+++ lib/screens/home/job/employment_info.dart
@@ -29,45 +29,27 @@ class EmploymentInfo extends StatelessWidget {
                   applyToJobHeader(context, jobName, 2,
                       "Employment Information", Icons.arrow_back_ios),
                   const SizedBox(height: 20.0),
-                  const Text(
-                    "Education",
-                    style: TextStyle(
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.w600,
-                        color: taupeColor),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "Included in Resume",
-                        style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w400,
-                            color: taupeColor),
-                      ),
-                      Container(
-                        padding: EdgeInsets.zero,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: taupeColor),
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: CupertinoSwitch(
-                          thumbColor: taupeColor,
-                          trackColor: soapStoneColor,
-                          value: false,
-                          activeColor: soapStoneColor,
-                          onChanged: (value) {},
-                        ),
-                      )
-                    ],
-                  ),
+                  headinInfo("Education", "Included in Resume", false),
                   const SizedBox(height: 20.0),
                   InputField(
                     onChanged: (nameOfSchool) {},
                     labelName: 'Name of school',
                     hintText: 'Horizon Education University',
-                  )
+                  ),
+                  const SizedBox(height: 10.0),
+                  InputField(
+                    onChanged: (course) {},
+                    labelName: 'Course of study',
+                    hintText: 'Data Analytics',
+                  ),
+                  const SizedBox(height: 10.0),
+                  InputField(
+                    onChanged: (course) {},
+                    labelName: 'Year graduated',
+                    hintText: '2012',
+                  ),
+                  const SizedBox(height: 10.0),
+                  headinInfo("Work Experience", "Included in Resume", true),
                 ],
               ),
             ),
@@ -77,3 +59,41 @@ class EmploymentInfo extends StatelessWidget {
     );
   }
 }
+
+Widget headinInfo(String heading, String subHeading, bool isToggleOn) => Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          heading,
+          style: const TextStyle(
+              fontSize: 25.0, fontWeight: FontWeight.w600, color: taupeColor),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              subHeading,
+              style: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w400,
+                  color: taupeColor),
+            ),
+            Container(
+              padding: EdgeInsets.zero,
+              decoration: BoxDecoration(
+                border: Border.all(color: taupeColor),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: CupertinoSwitch(
+                thumbColor: taupeColor,
+                trackColor: soapStoneColor,
+                value: isToggleOn,
+                activeColor: soapStoneColor,
+                onChanged: (value) {},
+              ),
+            )
+          ],
+        ),
+      ],
+    );
